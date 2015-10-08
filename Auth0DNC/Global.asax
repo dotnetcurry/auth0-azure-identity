ec04d1bff0c7d8468266e40fc20989af0cc05293
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2015-10-08 07:27:36 +0530

    azure-auth0

---
+++ Auth0DNC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Auth0DNC.MvcApplication" Language="C#" %>
---
+++ Auth0DNC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Auth0DNC.Startup))]
+namespace Auth0DNC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
